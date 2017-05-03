22921d65dcdc1df285c237f7220702dbf1bd5c8c
romaz <romaz@DESKTOP-9IJAD59>
2017-05-03 20:51:02 +0300

    Initial

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿//https://metanit.com/sharp/mvc5/24.4.php
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
